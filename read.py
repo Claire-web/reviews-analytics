5cdf7cb3de67c0f68dd52d8cb1e31d91e51ebcef
Claire-web <57494370+Claire-web@users.noreply.github.com>
2020-01-31 11:21:29 +0800

    calc avg len read.py

--- read.py
+++ read.py
@@ -6,10 +6,16 @@ with open('reviews.txt', 'r') as f:
         count = count + 1 # count += 1
         if count % 1000 == 0:
             print(len(data))
-print(len(data))
+print('檔案讀取完畢,總共有', len(data), '筆資料')
 
-print(data[0])
-print('--------')
-print(data[1])
-print('--------')
-print(data[2])
+#print(data[0])
+#print('--------')
+#print(data[1])
+#print('--------')
+#print(data[2])
+
+sum_len = 0
+for d in data: #每個d是一個字串
+    sum_len = sum_len + len(d)
+    print(sum_len)
+print('留言平均長度為', sum_len / len(data))
