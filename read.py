1ff082d0d853ba7cfa19914ada638887981f095a
Claire-web <57494370+Claire-web@users.noreply.github.com>
2020-02-19 11:27:33 +0800

    update read.py 20200219

--- read.py
+++ read.py
@@ -8,14 +8,26 @@ with open('reviews.txt', 'r') as f:
             print(len(data))
 print('檔案讀取完畢,總共有', len(data), '筆資料')
 
-#print(data[0])
-#print('--------')
-#print(data[1])
-#print('--------')
-#print(data[2])
-
 sum_len = 0
 for d in data: #每個d是一個字串
-    sum_len = sum_len + len(d)
-    print(sum_len)
+    sum_len = sum_len + len(d) # sum_len(d) += 1
 print('留言平均長度為', sum_len / len(data))
+
+
+new = []
+for d in data:   #把清單內的東西一筆一筆拿出來
+	if len(d) < 100:  #如果長度小於100
+		new.append(d) #就裝入新清單
+print('一共有', len(new), '筆留言長度小於100')  #印出留言長度小於100的筆數
+print(new[0])
+print('----------------')
+print(new[1])
+
+good = []
+for d in data:   #把清單內的東西一筆一筆拿出來
+	if 'good' in d:  #如果good這個自有在d裡面
+		good.append(d) #就裝入good清單
+print('一共有', len(good), '筆留言提到good')  #印出留言含good的筆數
+print(good[0])
+print('----------------')
+print(good[1])
